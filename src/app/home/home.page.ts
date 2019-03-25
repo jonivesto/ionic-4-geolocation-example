ac5633b0240d9695f9ea572be1810c5ee5e07d55
jonivesto <n7vejo00@students.oamk.fi>
2019-03-25 18:57:14 +0200

    Working example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+/**
+ * @author Joni-Pekka Vesto <https://www.jonivesto.com/>
+ * @since 25-03-2019
+ */
 
 @Component({
   selector: 'app-home',
@@ -6,5 +12,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(private geolocation: Geolocation) {}
+
+  // Run on page load
+  ngOnInit(){
+    // Get current location
+    this.geolocation.getCurrentPosition().then((response) => {
+      // Print response
+      console.log('Latitude: ', response.coords.latitude);
+      console.log('Longitude: ', response.coords.longitude);
+      // Handle errors
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
 
 }
